52d2ebdb0b8ce47822bfbbcd50e9141a75317247
evelisko <evelisko@mail.ru>
2024-11-25 23:33:13 +0400

    Вынес код основого окна в UserControl. Избавился от глоальных переменных.

--- video_player.py
+++ video_player.py
@@ -12,276 +12,199 @@ import numpy as np
 from video_viewer import VideoViewer
 from setting_panel import SettingsPanelNavigationDrawer
 
-current_frame = 0
-capture = None
-latency = None
-current_time = None
-total_time = None
-frame_count = None
-is_video_play = False
-is_change_position = False
-files_list_opened = False
-video_window_width = 0
 
-# TODO: Реализовать в виде отдельного класса.
-def main(page: ft.Page):
-    page.padding = 5
-    page.window.left = page.window.left + 100
-    page.theme_mode = ft.ThemeMode.LIGHT
+class MainControl(ft.UserControl):
+    def __init__(self) -> None:
+        super().__init__()
+        self.is_video_play = False
+        self.files_list_opened = False
+        self.video_window_width = 0
+        self.expand = True
 
-    def on_file_list_click(e): # 8ec182
+    def build(self) -> ft.Control:
+        self.end_drawer = SettingsPanelNavigationDrawer()
+
+        self.sldr_time_bar = ft.Slider(
+                            min=0, max=1000,
+                            divisions=1000,
+                            label="{value}", 
+                            active_color=ft.colors.PURPLE,
+                            secondary_active_color=ft.colors.RED,
+                            thumb_color=ft.colors.PURPLE,
+                            expand=True, 
+                            on_change=self.slider_changed)
+
+        self.btn_play = ft.IconButton(
+            icon=ft.icons.PLAY_ARROW, on_click=self.play_button_clicked, data=0
+        )
+        self.btn_next_video=ft.IconButton(
+            icon=ft.icons.SKIP_NEXT_ROUNDED,  # on_click=play_button_clicked, data=0
+        )
+        self.btn_prev_video = ft.IconButton(
+            icon=ft.icons.SKIP_PREVIOUS_ROUNDED,  # on_click=play_button_clicked, data=0
+        )
+        self.btn_settings = ft.IconButton(
+            icon=ft.icons.SETTINGS, on_click=lambda e: self.page.open(self.end_drawer)  # ^_^
+        )
+        self.btn_play_list = ft.IconButton(
+            icon=ft.icons.MENU, on_click=self.show_playlist
+        )    
+        self.btn_open_video = ft.IconButton(
+                                icon=ft.icons.FILE_UPLOAD_OUTLINED,
+                                on_click=self.on_click,  # lambda _: pick_files_dialog.pick_files(allow_multiple=True)
+                                tooltip='Open File'  # ft.Tooltip..TooltipValue 
+                            )
+        self.ft_time_line_value = ft.Text(value='00:00:00/00:00:00')  
+        self.ft_play_track_coontainer = ft.Container(content=ft.Row(
+            controls=[self.btn_prev_video, self.btn_play, self.btn_next_video, self.sldr_time_bar, self.ft_time_line_value, self.btn_open_video, self.btn_play_list, self.btn_settings], 
+            expand=True, 
+            alignment=ft.alignment.center_right
+            ),
+            # bgcolor=ft.colors.RED
+            )
+        #====================================================================
+        self.pick_files_dialog = ft.FilePicker(on_result=self.pick_files_result)
+        self.page.overlay.append(self.pick_files_dialog)
+        self.video_viewer = VideoViewer(update_frame_index=self.on_update_frame_index)
+        self.tf_page_size = ft.TextField(value='', expand=True)
+
+        self.ft_row = ft.Column([self.video_viewer, ft.Row([self.tf_page_size]), self.ft_play_track_coontainer],
+                            # width = page.width-10
+                            expand=True
+                            )
+        # Перенос видео через drug & drop.
+        #====================================================================
+        self.dt_files = ft.DataTable(
+                    columns=[
+                            ft.DataColumn(ft.Text("id")),
+                            ft.DataColumn(ft.Text("Name"))
+                        ],
+                    rows=[],
+                    )
+        self.gd = ft.GestureDetector(
+                    content=ft.VerticalDivider(),
+                    drag_interval=10,
+                    on_pan_update=self.move_vertical_divider,
+                    on_hover=self.show_draggable_cursor,
+                    visible=False
+                )
+        self.cnt = ft.Container(
+                    content=self.dt_files,
+                    # bgcolor='#53765f',
+                    alignment=ft.alignment.top_left,
+                    expand=True,
+                    visible=False
+                )
+        x_row = ft.Row(
+                controls=[
+                        self.ft_row,
+                        self.gd,
+                        self.cnt
+                ],
+            spacing=0,
+            expand=True
+        )
+        return x_row
+
+
+    def on_file_list_click(self, e): # 8ec182
         print(e.control.title.value)
         print(e.control.parent.item_extent)
 
         # Метод для заполнения списка файлов.
-    def read_files_list(files: List[FilePickerFile]):
-        dt_files.rows = []
-        dt_files.rows = list(map(lambda x: ft.DataRow(
+    def read_files_list(self, files: List[FilePickerFile]):
+        self.dt_files.rows = []
+        self.dt_files.rows = list(map(lambda x: ft.DataRow(
                                 cells=[
                                     ft.DataCell(ft.Text(x[0])),
                                     ft.DataCell(ft.Text(x[1].name))
                                 ],
                     selected=True,
-                    on_select_changed=on_file_list_click, #lambda e: print(f"row select changed: {e.data}"),
+                    on_select_changed=self.on_file_list_click, #lambda e: print(f"row select changed: {e.data}"),
                 ), enumerate(files) ))
-        dt_files.update()
+        self.dt_files.update()
 
-    def pick_files_result(e: ft.FilePickerResultEvent):
-        global current_frame
-        global capture
-        global latency
-        global frame_count
-        global is_video_play
-        global total_time
-
-        page.title  = (
-            ", ".join(map(lambda f: f.name, e.files)) if e.files else "Cancelled!"
-        )
+    def pick_files_result(self, e: ft.FilePickerResultEvent):
+        #--------------------------------------------------------------------------
+        # page.title  = (
+        #     ", ".join(map(lambda f: f.name, e.files)) if e.files else "Cancelled!"
+        # ) #Здесь сделаем вызов события потому передающего запись в заголовок окна.
+        #--------------------------------------------------------------------------
         if e.files:
-            read_files_list(e.files)
+            self.read_files_list(e.files)
             file_name = e.files[0].path
-            current_frame = 0
-            is_new = True
-            video_viewer.open_file(file_name=file_name) # Возвращает к-во кадров в видео или статус открытия видео.
-            frame_count = video_viewer.get_frames_count()
-            sldr_time_bar.max = frame_count
-            sldr_time_bar.divisions = frame_count
+            self.video_viewer.open_file(file_name=file_name) # Возвращает к-во кадров в видео или статус открытия видео.
+            frame_count = self.video_viewer.get_frames_count()
+            self.sldr_time_bar.max = frame_count
+            self.sldr_time_bar.divisions = frame_count
+            self.is_video_play = True
+            self.btn_play.icon = ft.icons.PAUSE
+        self.update()
 
-
-            # if capture:
-            #     if capture.isOpened():
-            #         capture.release()
-            #         capture=None
-            #         is_new = False
-            #         # thread.stop()
-            # capture = cv2.VideoCapture(file_name)
-            # latency = 1 / capture.get(cv2.CAP_PROP_FPS)
-            # frame_count = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
-            # # capture.set(cv2.CAP_PROP_POS_FRAMES, 100)
-            is_video_play = True
-            btn_play.icon = ft.icons.PAUSE
-            # sldr_time_bar.max = int(frame_count)
-            # sldr_time_bar.divisions = int(frame_count)
-            # total_time = time_to_str(c * latency) 
-            # print(total_time)
-            # if is_new:
-            #     thread = threading.Thread(target=update_frame, args=(), daemon=True)
-            #     thread.start()
-        page.update()
-
-    def time_to_str(seconds: float):
+    def time_to_str(self, seconds: float):
         dtime = timedelta(seconds = seconds)
         mm, ss = divmod(dtime.seconds, 60)
         hh, mm = divmod(mm, 60)
         return "%d:%02d:%02d" % (hh, mm, ss) if hh > 0 else "%02d:%02d" % (mm, ss)
     
-    # def on_image_viewer_mouse_move(x, y):
-    #     tf_page_size.value = f'{x}/{y}'
-    #     tf_page_size.update()
-
-    def on_update_frame_index(value):
-        sldr_time_bar.value = value
-        ft_time_line_value.value = f'{video_viewer.get_current_time()}/{video_viewer.get_total_time()}'
-        page.update()
+    def on_update_frame_index(self, value):
+        self.sldr_time_bar.value = value
+        self.ft_time_line_value.value = f'{self.video_viewer.get_current_time()}/{self.video_viewer.get_total_time()}'
+        self.update()
 
 
-    def on_click(e):
-        pick_files_dialog.pick_files(allow_multiple=True)
+    def on_click(self, e):
+        self.pick_files_dialog.pick_files(allow_multiple=True)
 
-    # def update_frame():
-    #     # TODO: https://all-python.ru/osnovy/threading.html
-    #     global current_frame
-    #     global latency
-    #     global frame_count
-    #     global capture
-    #     global is_change_position
-    #     # Реализовать просмотр и перемотку.
-    #     while current_frame < frame_count:
-    #         if capture: # Проверка что открыт новый файл.
-    #             if is_video_play:
-    #                 start_time = datetime.now()
-    #                 if is_change_position:
-    #                     capture.set(cv2.CAP_PROP_POS_FRAMES, int(current_frame))
-    #                     is_change_position = False
-    #                 retval, frame = capture.read()
-    #                 img_viewer.read_image(frame)
-    #                 sldr_time_bar.value = current_frame
-    #                 ft_time_line_value.value = f'{time_to_str(current_frame*latency)}/{total_time}'
-    #                 ft_time_line_value.update()
-    #                 tf_page_size.value = str(int(capture.get(cv2.CAP_PROP_POS_FRAMES)))
-    #                 tf_page_size.update() 
-    #                 current_frame += 1
-    #                 sldr_time_bar.update()
-    #                 d_time = datetime.now() - start_time
-    #             if d_time.microseconds/1000000 < latency and d_time.microseconds > 0.0:
-    #                sleep(latency - d_time.microseconds/1000000) # Здесь необходимо замерять время между вызовами, и если и ожидать только разницу.
-    #         else:
-    #             break
-    #     if capture:
-    #         capture.release()
-    
-    def on_page_resize(e):
-        tf_page_size.value = f'{page.width}/{page.height}'
-        page.update()
+    def slider_changed(self, e):
+        self.sldr_time_bar.label = self.time_to_str(e.control.value * self.video_viewer.get_frame_rate())
+        self.video_viewer.set_current_frame_index(int(e.control.value))
 
-    def slider_changed(e):
-        # global is_change_position
-        # current_frame = int(e.control.value)
-        # is_change_position = True
-        # capture.set(cv2.CAP_PROP_POS_FRAMES, int(e.control.value))
-        sldr_time_bar.label = time_to_str(e.control.value * video_viewer.get_frame_rate())
-        video_viewer.set_current_frame_index(int(e.control.value))
-
-    def play_button_clicked(e):
-        global is_video_play
-        if btn_play.icon == ft.icons.PAUSE:
-            btn_play.icon = ft.icons.PLAY_ARROW
-            is_video_play = False
+    def play_button_clicked(self, e):
+        if self.btn_play.icon == ft.icons.PAUSE:
+            self.btn_play.icon = ft.icons.PLAY_ARROW
+            self.is_video_play = False
         else:
-            btn_play.icon = ft.icons.PAUSE
-            is_video_play = True
-        video_viewer.change_play_status(is_video_play)
-        btn_play.update()
+            self.btn_play.icon = ft.icons.PAUSE
+            self.is_video_play = True
+        self.video_viewer.change_play_status(self.is_video_play)
+        self.btn_play.update()
 
-    def move_vertical_divider(e: ft.DragUpdateEvent):
-        # if (e.delta_x > 0 and ft_row.width < 300) or (e.delta_x < 0 and ft_row.width > 100):
-        ft_row.width += e.delta_x
-        ft_row.update()
+    def move_vertical_divider(self, e: ft.DragUpdateEvent):
+        self.ft_row.width += e.delta_x
+        self.ft_row.update()
 
-    def show_draggable_cursor(e: ft.HoverEvent):
+    def show_draggable_cursor(self, e: ft.HoverEvent):
         e.control.mouse_cursor = ft.MouseCursor.RESIZE_LEFT_RIGHT
         e.control.update()
 
-    def show_playlist(e):
-        global files_list_opened
-        global video_window_width
-
-        if video_window_width == 0:
-            video_window_width = page.width - 200
-
-        if not files_list_opened:
-            files_list_opened = True
-            ft_row.expand = False
-            ft_row.width = video_window_width
-            gd.visible =True
-            cnt.visible = True
+    def show_playlist(self, e):
+        if self.video_window_width == 0:
+            self.video_window_width = self.page.width - 200
+        if not self.files_list_opened:
+            self.files_list_opened = True
+            self.ft_row.expand = False
+            self.ft_row.width = self.video_window_width
+            self.gd.visible =True
+            self.cnt.visible = True
         else:
-            files_list_opened = False
-            video_window_width = ft_row.width
-            ft_row.expand = True
-            gd.visible =False
-            cnt.visible = False
-        page.update()
+            self.files_list_opened = False
+            self.video_window_width = self.ft_row.width
+            self.ft_row.expand = True
+            self.gd.visible =False
+            self.cnt.visible = False
+        self.update()
 
-    end_drawer = SettingsPanelNavigationDrawer()
 
-    sldr_time_bar = ft.Slider(
-                        min=0, max=1000,
-                        divisions=1000,
-                        label="{value}", 
-                        active_color=ft.colors.PURPLE,
-                        secondary_active_color=ft.colors.RED,
-                        thumb_color=ft.colors.PURPLE,
-                        expand=True, 
-                        on_change=slider_changed)
-
-    btn_play = ft.IconButton(
-        icon=ft.icons.PLAY_ARROW, on_click=play_button_clicked, data=0
-    )
-    btn_next_video=ft.IconButton(
-        icon=ft.icons.SKIP_NEXT_ROUNDED,  # on_click=play_button_clicked, data=0
-    )
-    btn_prev_video = ft.IconButton(
-        icon=ft.icons.SKIP_PREVIOUS_ROUNDED,  # on_click=play_button_clicked, data=0
-    )
-    btn_settings = ft.IconButton(
-        icon=ft.icons.SETTINGS, on_click=lambda e: page.open(end_drawer)
-    )
-    btn_play_list = ft.IconButton(
-        icon=ft.icons.MENU, on_click=show_playlist
-    )    
-    btn_open_video = ft.IconButton(
-                            icon=ft.icons.FILE_UPLOAD_OUTLINED,
-                            on_click=on_click,  # lambda _: pick_files_dialog.pick_files(allow_multiple=True)
-                            tooltip='Open File'  # ft.Tooltip..TooltipValue 
-                        )
-    ft_time_line_value = ft.Text(value='00:00:00/00:00:00')  
-    ft_play_track_coontainer = ft.Container(content=ft.Row(
-        controls=[btn_prev_video, btn_play, btn_next_video, sldr_time_bar, ft_time_line_value, btn_open_video, btn_play_list, btn_settings], 
-        expand=True, 
-        alignment=ft.alignment.center_right
-        ),
-        # bgcolor=ft.colors.RED
-        )
-    #====================================================================
-    pick_files_dialog = ft.FilePicker(on_result=pick_files_result)
-    page.overlay.append(pick_files_dialog)
-    video_viewer = VideoViewer(update_frame_index=on_update_frame_index)
-    tf_page_size = ft.TextField(value='', expand=True)
-
-    ft_row = ft.Column([video_viewer, ft.Row([tf_page_size]), ft_play_track_coontainer],
-                        # width = page.width-10
-                        expand=True
-                        )
-    # Перенос видео через drug & drop.
-    #====================================================================
-    dt_files = ft.DataTable(
-                  columns=[
-                        ft.DataColumn(ft.Text("id")),
-                        ft.DataColumn(ft.Text("Name"))
-                      ],
-                  rows=[],
-                )
-    gd = ft.GestureDetector(
-                content=ft.VerticalDivider(),
-                drag_interval=10,
-                on_pan_update=move_vertical_divider,
-                on_hover=show_draggable_cursor,
-                visible=False
-            )
-    cnt = ft.Container(
-                content=dt_files,
-                # bgcolor='#53765f',
-                alignment=ft.alignment.top_left,
-                expand=True,
-                visible=False
-            )
-    x_row = ft.Row(
-            controls=[
-                    ft_row,
-                    gd,
-                    cnt
-            ],
-        spacing=0,
-        expand=True
-    )
-    #====================================================================
-    page.add(x_row)
-    page.on_resized = on_page_resize
+# TODO: Реализовать в виде отдельного класса.
+def main(page: ft.Page):
+    page.add(MainControl()) # Вызов события и параметров контрола.
+    # page.on_resized = on_page_resize
+    page.padding = 5
+    page.window.left = page.window.left + 100
+    page.theme_mode = ft.ThemeMode.LIGHT
     
 
 if __name__ == '__main__':
     ft.app(target=main)
-
-# Циклическое воспроизведение видео.
     
